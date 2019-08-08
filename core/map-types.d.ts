2725733a243c5510e9f54cfd3fa3ffeb884ff307
Robin Schmidt <schmidt91robin@icloud.com>
2019-08-08 12:31:57 +0200

    init

--- core/map-types.d.ts
+++
@@ -1,9 +0,0 @@
-import { LatLngLiteral } from './services/google-maps-types';
-export { DataMouseEvent, KmlMouseEvent, LatLngBounds, LatLngBoundsLiteral, LatLngLiteral, MarkerLabel, PolyMouseEvent } from './services/google-maps-types';
-/**
- * MouseEvent gets emitted when the user triggers mouse events on the map.
- */
-export interface MouseEvent {
-    coords: LatLngLiteral;
-    placeId?: string;
-}
